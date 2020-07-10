70c52f8b8a3038aefdeacd68819dfe5fa8566992
amy choi <cgc8016@ewhain.net>
2020-07-11 02:24:13 +0900

    add DBConfig struct for DB connect only once

---
+++ controller/common.go
@@ -0,0 +1,35 @@
+package controller
+
+import (
+	"database/sql"
+
+	"github.com/pkg/errors"
+
+	"github.com/AMYMEME/board-golang/config"
+
+	"github.com/AMYMEME/board-golang/repository"
+)
+
+var logger = config.GetLogger()
+
+var DB = repository.DBConfig{
+	MyDB: setupDB(),
+}
+
+func setupDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:root@(127.0.0.1:3306)/board")
+	if err != nil {
+		logger.Errorf("Fail DB connection")
+	}
+
+	return db
+}
+
+func connectionCheck(db *sql.DB) error {
+	if db == nil {
+		err := errors.New("Fail sql driver connection")
+		logger.Errorf(err.Error())
+		return err
+	}
+	return nil
+}
--- repository/config.go
+++ repository/config.go
@@ -2,9 +2,12 @@ package repository
 
 import (
 	"database/sql"
+
 	"github.com/AMYMEME/board-golang/config"
 )
 
-var DB, _ = sql.Open("mysql", "root:root@(127.0.0.1:3306)/board")
-var Logger = config.GetLogger()
+type DBConfig struct {
+	MyDB *sql.DB
+}
 
+var logger = config.GetLogger()
