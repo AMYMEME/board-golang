5bfbc3966e850c715b3a91a4ef0badb9a7d9443f
amy choi <cgc8016@ewhain.net>
2020-07-28 22:34:18 +0900

    for naver auth fe

--- view/src/components/page/Login.vue
+++ view/src/components/page/Login.vue
@@ -8,8 +8,8 @@
         <b-card class="col-sm-6">
           <b-button pill block variant="outline-danger" @click="loginWithGoogle" >
             Login with Google</b-button>
-          <b-button pill block variant="outline-success" @click="" >
-            Login with Naver</b-button>
+          <div id="naverIdLogin" class="mt-4"></div>
+
         </b-card>
         </b-row>
       </div>
@@ -22,7 +22,6 @@
     name: 'test',
     methods: {
       loginWithGoogle() {
-
         this.$gAuth.getAuthCode()
                 .then(authCode => {
                   this.$store.dispatch('loginWithGoogle', {authCode})
@@ -40,8 +39,42 @@
         } else {
           this.$router.push(redirectURI)
         }
-      }
-    }
+      },
+    },
+    mounted() {
+      var naverLogin = new naver.LoginWithNaverId(
+              {
+                clientId: `_HXiSkrZSfB9vq94TsZC`,
+                callbackUrl: `http://localhost:8080/auth/naver`,
+                isPopup: true,
+                callbackHandle: true,
+                loginButton: {color: "green", type: 3, height: 50}
+              }
+      );
+
+      naverLogin.init();
+      window.addEventListener('load', function () {
+        naverLogin.getLoginStatus(function (status) {
+          if (status) {
+            /* 필수적으로 받아야하는 프로필 정보가 있다면 callback처리 시점에 체크 */
+            var email = naverLogin.user.getEmail();
+            var name = naverLogin.user.getNickName();
+
+            if( email == undefined || email == null || name == undefined || name == null) {
+              alert("이메일은 필수정보입니다. 정보제공을 동의해주세요.");
+              naverLogin.reprompt();
+              return;
+            }
+
+            alert(email + name); //for debugging
+
+            this.redirect();
+          } else {
+            console.log("callback 처리에 실패하였습니다.");
+          }
+        });
+      });
+    },
   }
   function getUrlParams() {
     var params = {};
